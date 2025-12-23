36dbb67dc890de92d5d5a165026a0866f890dee4
Muhammad <Hammad90191@gmail.com>
2025-12-23 10:41:17 +0500

    Reimplemented cartslice.js

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,99 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
-const recalculateTotals = (state) => {
-  state.totalQuantity = state.products.reduce((acc, p) => acc + p.quantity, 0);
-  state.totalPrice = state.products.reduce((acc, p) => acc + p.price * p.quantity, 0);
-  state.shipping = state.totalPrice > 15000 ? 0 : 1000;
-  state.total=state.totalPrice+state.shipping;
-};
-
+const recalculateTotal=(state)=>{
+  state.totalPrice=state.products.reduce((total , current)=>total+current.price * current.quantity,0);
+  state.totalQuantity=state.products.reduce((total , current)=>total+current.quantity,0);
+}
 const cartSlice = createSlice({
-  name: "cart",
-  initialState: {
-    products: [],
-    totalQuantity: 0,
-    totalPrice: 0,
-    actualTotal:0,
-    shipping:0,
-    total:0
+  name:"cart",
+  initialState:
+  {
+    products:[],
+    totalPrice:0,
+    totalQuantity:0
   },
-  reducers: {
-    addProduct(state, action) {
+  reducers:{
+    addProduct(state,action){
       const product = action.payload;
-      const existing = state.products.find((p) => p._id === product._id);
-      if (existing && existing.quantity<=existing.stock) {
-        existing.quantity += 1;
-      } else {
-        state.products.push({ ...product, quantity: product.quantity || 1 });
+      const exist = state.products.find((prod)=>prod._id===product._id);
+      if (exist && product.stock > product.quantity) {
+        exist.quantity+=quantity;
       }
-      recalculateTotals(state);
+      state.products.push({...product,quantity:1});
+      recalculateTotal(state);
     },
-    addProductByQuantity(state, action) {
-      const {product,quantity} = action.payload;
-      const existing = state.products.find((p) => p._id === product._id);
-
-      if (existing && existing.quantity<=existing.stock) {
-        existing.quantity += quantity;
-      } else {
-        state.products.push({ ...product, quantity: quantity });
+    decreaseQuantity(state,action){
+      const {id} = action.payload;
+      const exist = state.products.find((prod)=>prod._id===id);
+      if (exist && exist.quantity > 0) {
+        exist.quantity-=1;
       }
-recalculateTotals(state);
-      // state.totalQuantity += quantity;
-      // state.totalPrice += product.price * (quantity);
+      recalculateTotal(state);
     },
-
-    removeProduct(state, action) {
-      const id = action.payload._id;
-      // const existing = state.products.find((p) => p._id === id);
-      if (existing) {
-        // state.totalQuantity -= existing.quantity;
-        // state.totalPrice -= existing.price * existing.quantity;
-        state.products = state.products.filter((product) => product._id !== id);
+    increaseQuantity(state,action){
+      const {id} = action.payload;
+      const exist = state.products.find((prod)=>prod._id===id);
+      if (exist && exist.stock > exist.quantity) {
+        exist.quantity+=1;
       }
-      recalculateTotals(state);
+      recalculateTotal(state);
     },
-
-    updateQuantity(state, action) {
-      const { id, quantity } = action.payload;
-      const product = state.products.find((p) => p._id === id);
-      if (product && quantity <= product.stock && quantity >= 1) {
-        product.quantity = quantity;
-      }
-      recalculateTotals(state);
-      // if (product) {
-      //   const quantityDifference = quantity - product.quantity;
-      //   product.quantity = quantity;
-      //   state.totalQuantity += quantityDifference;
-      //   state.totalPrice += quantityDifference * product.price;
-      // }
-    },
-    increaseQuantity(state, action){
-      console.log('first called')
-      const {id}=action.payload;
-      const product = state.products.find((p) => p._id === id);
-      console.log('first', state.products)
-      if (product && product.quantity<product.stock) {
-        product.quantity+=1;
-      }
-      recalculateTotals(state);
-    },
-    decreaseQuantity(state, action){
-      const {id}=action.payload;
-      const product = state.products.find((p) => p._id === id);
-      if (product && product.quantity>1) {
-        product.quantity-=1;
-      }else if (product.quantity === 1) {
-      state.products = state.products.filter((p) => p._id !== id);
-    }
-    recalculateTotals(state);
-    },
-
-    clearCart(state) {
-      state.products = [];
-      state.totalQuantity = 0;
-      state.totalPrice = 0;
-    },
-  },
-});
-
-export const { addProduct, removeProduct,addProductByQuantity, updateQuantity, clearCart,increaseQuantity,decreaseQuantity } = cartSlice.actions;
+  }
+})
+export const {addProduct, increaseQuantity, decreaseQuantity}=cartSlice.actions;
 export default cartSlice.reducer;
